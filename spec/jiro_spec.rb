c95210e9aa2dbcb09b118023f0f367c20a2d2d2f
Marcin Naglik <marcin.naglik@gmail.com>
2013-06-17 16:11:13 +0200

    adding specs for limit to handler config

--- spec/jiro_spec.rb
+++ spec/jiro_spec.rb
@@ -1,14 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Jiro" do
-  # Clearance.configure do |config|
-  #   config.mailer_sender = 'donotreply@example.com'
-  # end
-
-  # Clearance.configuration.mailer_sender= 'new.email@example.com'
-
-  # Clearance.configuration.mailer_sender
-
   it "defines default application id" do
     Jiro.configuration.app_id.should eq("jiro_cms_pages")
   end
@@ -25,5 +17,14 @@ describe "Jiro" do
   it "allows to change redis opts" do
     Jiro.configuration.redis_opts = { :port => 999 }
     Jiro.configuration.redis_opts.should eq({ :port => 999})
+  end
+
+  it "defines default limit to handlers" do
+    Jiro.configuration.limit_to_handlers.should eq(nil)
+  end
+
+  it "allows to change limit to handlers" do
+    Jiro.configuration.limit_to_handlers = [:erb]
+    Jiro.configuration.limit_to_handlers.should eq([:erb])
   end
 end
